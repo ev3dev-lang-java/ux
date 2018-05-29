d543a945991d01a429378a47b64e16b83be175d8
Sergio Ortiz Crespo <sergiooc@gmail.com>
2018-05-29 12:42:12 +0200

    Delete video content

--- src/js/core/ev3.js
+++ src/js/core/ev3.js
@@ -5,7 +5,6 @@
  * Behaviours supported & required *
  * - menuMobile
  * - Slider
- * - Link Video
 
  * @copyright (c) 2018-2019 Ev3 All Rights Reserved.
  * @date   29/05/2018
@@ -62,70 +61,3 @@ function slider(){
     ]
   });
 };
-
-/**
-* @function
-* @name Link Video
-* @description  Launch video player in website
-*/
-
-function playYoutube(){
-    var player, iframe, state, $popup = $('.popup');
-    $('.video_des a').click(function(e) {
-
-        e.preventDefault; 
- 
-
-        player = new YT.Player('youtubePlayer',{
-            videoId: $(this).data("video"),
-            width: "100%",
-            height: "100%",
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            },
-            playerVars: {
-                fs: 1,
-                autoplay: 1,
-                controls: 1,
-                info: 0,
-                rel: 0,
-                disablekb: 0,
-                wmode: 'transparent'
-            }
-        });
-
-    });
-
-    
-    //$('.popup span, .player_shadow').on('click', function(e){
-    // $('.popup span').on('click', function(e){
-    //   e.preventDefault;
-    //   $('body').removeClass('view_player');
-    //   $('.popup').removeClass('leave');
-    //   $('.popup, .player_shadow').hide();
-    //   $('.v_video').html('<div id="youtubePlayer"></div>');
-    // });
-    
-
-    function onPlayerReady(event) {
- 
-        $('body').addClass('view_player');
-        $('.popup').remove();
-        $('.popup').show();
-            
-        
-        iframe = $('#youtubePlayer');
-        event.target.playVideo();
-        
-    }
-
-    function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.ENDED) {
-            $('body').removeClass('view_player');
-            $('.player_shadow, .popup').hide();
-            $('.v_video').html('<span></span></a><div id="youtubePlayer"></div>');
-        }
-    }
-
-}
