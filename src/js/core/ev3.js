b3c6abf7ebacf5076c51ba7518c5f071e724b166
Sergio Ortiz Crespo <sergiooc@gmail.com>
2018-07-03 23:23:11 +0200

    submenu

--- src/js/core/ev3.js
+++ src/js/core/ev3.js
@@ -4,9 +4,10 @@
  *
  * Behaviours supported & required *
  * - menuMobile
+ * - submenuMobile
 
  * @copyright (c) 2018-2019 Ev3 All Rights Reserved.
- * @date   06/06/2018
+ * @date   03/07/2018
  * @requires jQuery
  */
 
@@ -20,6 +21,30 @@
 function menuMobile(){
  $('.btn_hamb').on('click', function(){
     var _parent = $(this).parent();
+
+    if (_parent.hasClass('open')){
+     $('body, .overlay').removeClass('lock');
+     _parent.removeClass('open');
+
+    }else{
+      $('body, .overlay').addClass('lock');
+      _parent.addClass('open');
+    }
+
+  });
+
+}
+
+/**
+* @function
+* @name submenuMobile
+* @description  Interaction submenu in mobile
+*/
+
+
+function submenuMobile(){
+ $('.btn_submenu').on('click', function(){
+    var _parent = $('.submenu');
 
     if (_parent.hasClass('open')){
      $('body, .overlay').removeClass('lock');
